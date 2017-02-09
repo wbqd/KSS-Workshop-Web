93798623ae39e71f8c6c62f4a7502d7c381da5f8
Joshua Jung <wbqdinno@gmail.com>
2017-02-09 21:47:14 +0900

    IndexCtrl을 추가한다.

---
+++ app/scripts/controllers/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name simWorkshop2017App.controller:IndexCtrl
+ * @description
+ * # IndexCtrl
+ * Controller of the simWorkshop2017App
+ */
+angular.module('simWorkshop2017App')
+  .controller('IndexCtrl', function ($scope, $mdSidenav, sideNavMenu) {
+    $scope.openLeftMenu = function() {
+      $mdSidenav('left').toggle();
+    };
+    $scope.menu = sideNavMenu;
+  });
