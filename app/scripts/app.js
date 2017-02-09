8afe767baa59661e469898d12126366b7e636030
Joshua Jung <wbqdinno@gmail.com>
2017-02-09 15:03:38 +0900

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name simWorkshop2017App
+ * @description
+ * # simWorkshop2017App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('simWorkshop2017App', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name simWorkshop2017App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the simWorkshop2017App
+ */
+angular.module('simWorkshop2017App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
